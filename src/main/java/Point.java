ab2c3a19bae8447c611ae14ac2aa18ac23126417
Bridonneau Vincent <vincent.bridonneau@enseirb-matmeca.fr>
2019-12-09 14:31:34 +0100

    Classe point

---
+++ src/main/java/Point.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Point {
+  List<Double> coords = new ArrayList<Double>();
+
+  public Point(int n) {
+    for (int i = 0; i < n; i++) {
+      coords.add(0.);
+    }
+  }
+
+  public int size() {
+    return coords.size();
+  }
+
+  public Point(List<Double> _coords) {
+    coords.addAll(_coords);
+  }
+
+  public Point(Point pt) {
+    this.coords.clear();
+    this.coords.addAll(pt.coords);
+  }
+
+  private Point addCoords(Point pt) {
+    int len = coords.size();
+    for(int i = 0; i < len; ++i) {
+      double sum = (pt.coords.get(i) + coords.get(i);
+      coords.set(i, sum);
+    }
+    return this;
+  }
+
+  public void scale(double a) {
+    int len = coords.size();
+    for (int i = 0; i < len; i++) {
+      coords.set(i, coords.get(i) * a);
+    }
+  }
+
+  public static Point add(Point lhs, Point rhs) {
+    Point res = new Point(lhs);
+    return res.addCoords(rhs);
+  }
+
+  public static Point sum(List<Point> pts) {
+    int ncoords = pts.get(0).coords.size();
+    Point res = new Point(ncoords);
+    for(Point pt : pts) {
+      res = Point.add(res, pt);
+    }
+    return res;
+  }
+
+  public static Point mean(List<Point> pts) {
+    Point res = Point.sum(pts);
+    res.scale(1. / pts.size());
+    return res;
+  }
+}
