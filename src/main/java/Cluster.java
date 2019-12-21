babfefa9bfedb28ca905777b586f6a3b2a785d2b
Arno Galvez <arno.galvez@gmail.com>
2019-12-21 14:01:41 +0100

    Add KmeansReducer, Modify Cluster

--- src/main/java/Cluster.java
+++ src/main/java/Cluster.java
@@ -1,6 +1,11 @@
-import javax.ws.rs.Path;
+import org.apache.hadoop.io.Writable;
 
-public class Cluster {
+import javax.ws.rs.Path;
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class Cluster implements Writable {
     private int id;
     Cluster(int id)
     {
@@ -15,5 +20,17 @@ public class Cluster {
     public static void InitClusterFile(Path path, int k)
     {
 
+    }
+
+    @Override
+    public void write(DataOutput out) throws IOException
+    {
+        out.writeInt( id );
+    }
+
+    @Override
+    public void readFields(DataInput in) throws IOException
+    {
+        id = in.readInt();
     }
 }
